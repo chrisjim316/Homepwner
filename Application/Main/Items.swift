605b80fb75ef5e98c0bc03af723c4d7999b41a27
Chris Jim <chrisjim316@gmail.com>
2017-06-19 22:55:09 +0800

    Create Items.swift

---
+++ Application/Main/Items.swift
@@ -0,0 +1,26 @@
+//
+//  Items.swift
+//  Homepwner
+//
+//  Created by Chris Jim on 19/06/2017.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int){
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+    
+}
