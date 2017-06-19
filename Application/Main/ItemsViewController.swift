08f4c1c39d2bd71a4c6b494179e04f310c550591
Chris Jim <chrisjim316@gmail.com>
2017-06-19 22:55:29 +0800

    Create ItemsViewController.swift

---
+++ Application/Main/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Chris Jim on 19/06/2017.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController : UITableViewController {
+    
+}
